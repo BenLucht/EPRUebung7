4c7ade0b4b22b7a52349e5fdfe1beec67393d52d
BenLucht <FinanceResearchLab@24dcaea7d349>
2018-01-30 10:40:03 +0000

    add empty tree module

---
+++ tree.py
@@ -0,0 +1,11 @@
+"""
+Implements binary tree for EPR7 exercises.
+"""
+
+__author__ = "xxxxxxx: Ben, xxxxxxx: Anne"
+__copyright__ = "Copyright 2017/2018 – EPR-Goethe-Uni"
+__credits__ = "We would like to thank our coffee for the morning motivation."
+__email__ = "xxxxxxxx@stud.uni-frankfurt.de, xxxxxxxx@stud.uni-frankfurt.de"
+
+class tree():
+    pass
