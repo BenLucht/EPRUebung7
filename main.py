f946f7d7abac38128018745caefa0c35601f9733
BenLucht <FinanceResearchLab@24dcaea7d349>
2018-01-30 10:37:44 +0000

    add empty main module

---
+++ main.py
@@ -0,0 +1,13 @@
+"""
+Main Module for EPR7 exercises.
+"""
+
+from text import text
+
+__author__ = "xxxxxxx: Ben, xxxxxxx: Anne"
+__copyright__ = "Copyright 2017/2018 – EPR-Goethe-Uni"
+__credits__ = "We would like to thank our coffee for the morning motivation."
+__email__ = "xxxxxxxx@stud.uni-frankfurt.de, xxxxxxxx@stud.uni-frankfurt.de"
+
+if __name__ == '__main__':
+    pass
